9b763d3dc591ef22b50df643be57d8d4b0173cae
Rohankumar Patel <rohanpatel@Rohankumars-MacBook-Air.local>
2021-12-06 15:25:56 -0500

    manually updating the main branch with Google_API_setup branch

---
+++ server/controllers/dbControllers.js
@@ -0,0 +1,50 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const getClientID = (res, req) => {
+    
+}
+
+export const createClientID = (res, req) => {
+    
+}
+
+export const getTodolist = (res, req) => {
+    
+}
+
+export const createTodolist = (res, req) => {
+    
+}
+
+export const getNote = (res, req) => {
+    
+}
+
+export const createNote = (res, req) => {
+    
+}
+
+// export const getCalender = (res, req) => {
+    
+// }
+
+// export const createCalender = (res, req) => {
+    
+// }
+
+export const getQuicklink = (res, req) => {
+    
+}
+
+export const createQuicklink = (res, req) => {
+    
+}
+
+export const getEvent = (res, req) => {
+    
+}
+
+export const createEvent = (res, req) => {
+    
+}
