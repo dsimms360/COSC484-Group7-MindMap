10986fc7619597befff9af0a31657ec393f64d82
Rohankumar Patel <rohanpatel@Rohankumars-MacBook-Air.local>
2021-12-06 11:48:35 -0500

    added format for event control block

--- client/src/components/eventsBlock/eventsBlocks.jsx
+++ client/src/components/eventsBlock/eventsBlocks.jsx
@@ -13,3 +13,46 @@ export const Card = styled.div`
   border-radius: 8px;
 
 `;
+
+export const HeadingBlock = styled.div`
+  position: absolute;
+  left: 4.0%;
+  right: 2.00%;
+  top: 2.00%;
+  bottom: 89.00%;
+
+  font-family: "Roboto";
+  font-style: normal;
+  font-size: large;
+  // font-weight: normal;
+  font-size: x-large;
+  
+
+  // for placement test
+  background: purple;
+
+`;
+
+export const ButtonBlock = styled.div`
+  position: absolute;
+  top: 12%;
+  bottom: 75%;
+  right: 2.00%;
+  left: 4.00%;
+
+  
+
+  // for placement test
+  background: purple;
+`;
+
+export const FormBlock = styled.div`
+  position: absolute;
+  left: 4.0%;
+  right: 2.0%;
+  top: 26.0%;
+  bottom: 2.0%;
+
+  // for placement test
+  background: purple;
+`;
--- client/src/components/eventsBlock/index.jsx
+++ client/src/components/eventsBlock/index.jsx
@@ -1,9 +1,18 @@
-import { Card } from "./eventsBlocks";
+import { 
+    Card,
+    HeadingBlock,
+    ButtonBlock,
+    FormBlock
+} from "./eventsBlocks";
 
 function EventsBlock (){
     return(
         <>
-        <Card><br/>Add events form will be here</Card>
+        <Card>
+            <HeadingBlock>Event Controls:</HeadingBlock>
+            <ButtonBlock>Add button &emsp; Edit button &emsp; Delete button</ButtonBlock>
+            <FormBlock>Respective forms will be here. Note: the purple background will be removed</FormBlock>
+        </Card>
         </>
     );
 
